8431724609b311f7e44944d3fbe561f906ed3e39
Alexey Ozeritst <aozeritsky@gmail.com>
2009-03-05 12:32:37 +0300

    --

--- tests/test_laplace.cpp
+++ tests/test_laplace.cpp
@@ -48,14 +48,14 @@ void usage(const char * name)
 
 double rp(double x, double y)
 {
-	//return -2.0 * M_PI * M_PI * sin(M_PI * x) * sin(M_PI * y);
-	return 2.0 * y * y - 2.0 * y + 2.0 * x * x - 2.0 * x;
+	return -2.0 * M_PI * M_PI * sin(M_PI * x) * sin(M_PI * y);
+	//return 2.0 * y * y - 2.0 * y + 2.0 * x * x - 2.0 * x;
 }
 
 double ans(double x, double y)
 {
-	//return sin(M_PI * x) * sin(M_PI * y);
-	return x * (x - 1) * y * (y - 1);// + 1.0;
+	return sin(M_PI * x) * sin(M_PI * y) + 1.0;
+	//return x * (x - 1) * y * (y - 1);// + 1.0;
 }
 
 double bnd(double x, double y)
