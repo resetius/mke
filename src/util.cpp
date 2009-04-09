81d4dd668f8be7aa59c6615e29cd15d307317d6b
Alexey Ozeritsky <aozeritsky@gmail.com>
2009-04-09 19:02:39 +0400

    --

--- src/util.cpp
+++ src/util.cpp
@@ -386,6 +386,39 @@ void set_fpe_except()
 	_controlfp(cw, MCW_EM);
 }
 #else
+
+
+/*
+	For reference, the layout of the MXCSR register:
+	FZ:RC:RC:PM:UM:OM:ZM:DM:IM:Rsvd:PE:UE:OE:ZE:DE:IE
+	15 14 13 12 11 10  9  8  7   6   5  4  3  2  1  0
+
+	And the layout of the 387 FPU control word register:
+	Rsvd:Rsvd:Rsvd:X:RC:RC:PC:PC:Rsvd:Rsvd:PM:UM:OM:ZM:DM:IM
+	 15   14   13 12 11 10  9  8   7    6   5  4  3  2  1  0
+
+	Where:
+		Rsvd - Reserved
+		FZ   - Flush to Zero
+		RC   - Rounding Control
+		PM   - Precision Mask
+		UM   - Underflow Mask
+		OM   - Overflow Mask
+		ZM   - Zerodivide Mask
+		DM   - Denormal Mask
+		IM   - Invalid Mask
+		PE   - Precision Exception
+		UE   - Underflow Exception
+		OE   - Overflow Exception
+		ZE   - Zerodivide Exception
+		DE   - Denormal Exception
+		IE   - Invalid Exception
+		X    - Infinity control (unused on 387 and higher)
+		PC   - Precision Control
+
+	Source: Intel Architecture Software Development Manual, Volume 1, Basic Architecture
+*/
+
 void set_fpe_except()
 {
 	//http://www.website.masmforum.com/tutorials/fptute/fpuchap1.htm
@@ -400,6 +433,9 @@ void set_fpe_except()
 	m |= (1 << 9); // double precision
 	asm ("fldcw %0" : : "m" (*&m));
 	asm ("fwait");
+
+	m = 0;
+	asm ("ldmxcsr %0" : : "m" (*&m));
 }
 #endif
 
