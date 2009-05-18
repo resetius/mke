f12193076b71b3f2bdd4b7441d65813676ee5598
Alexey Ozeritsky <aozeritsky@gmail.com>
2009-05-18 19:57:47 +0400

    --

--- scripts/test_laplace.lua
+++ scripts/test_laplace.lua
@@ -1,9 +1,12 @@
 #!/usr/bin/lua
-path = arg[0]
+path = arg[1]
 exe1 = path .. "/test_laplace"
 exe2 = path .. "/rectangle"
+print(path)
 os.execute(exe2 .. " 0 0 1 1 3 > r3.txt")
 os.execute(exe2 .. " 0 0 1 1 4 > r4.txt")
 os.execute(exe2 .. " 0 0 1 1 5 > r5.txt")
-f    = io.popen(exe .. " r3", r)
+f = io.popen(exe1 .. " r3.txt", r)
+f = io.popen(exe1 .. " r4.txt", r)
+f = io.popen(exe1 .. " r5.txt", r)
 
