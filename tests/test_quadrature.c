dc0a40f2de21dfd4ec5782eac85fc09547bfb5d1
Alexey Ozeritsky <aozeritsky@gmail.com>
2009-05-18 15:10:09 +0400

    --

--- tests/test_quadrature.c
+++ tests/test_quadrature.c
@@ -14,15 +14,26 @@ double f2(double x, void * unused)
 	return x / cos(x);
 }
 
+void check(double a, double b)
+{
+	if (fabs(a - b) > 1e-14) {
+		fprintf(stderr, "%.16lf != %.16lf\n", a, b);
+		exit(-1);
+	}
+}
+
 int main()
 {
 	double i;
 
 	i = gauss_kronrod15(0, 1, f1, 0);
-	printf("int 0 1 x * x = %.16le\n", i);
+	fprintf(stderr, "int 0 1 x * x = %.16le\n", i);
+	check(i, 0.3333333333333333333);
+
 
 	i = gauss_kronrod15(-1, 1, f2, 0);
-	printf("int -1 1 x / cos(x) = %.16le\n", i);
+	fprintf(stderr, "int -1 1 x / cos(x) = %.16le\n", i);
+	check(i, 0);
 	return 0;
 }
 
