4f8564479fbf98a598e6794b67126ef27bc6ab03
Alexey Ozeritsky <aozeritsky@gmail.com>
2009-06-02 16:29:09 +0400

    --

--- utils/netcdf_cmd.h
+++ utils/netcdf_cmd.h
@@ -1,7 +1,15 @@
 #ifndef NETCDF_CMD_H
 #define NETCDF_CMD_H
 
+#include <string.h>
+
 struct CMD_Parser {
+	int cur_;
+	int argc_;
+	char ** argv_;
+
+	CMD_Parser (int argc, char * argv[]): cur_(0), argc_(argc), argv_(argv) {}
+	int next();
 };
 
 int yyparse(CMD_Parser * );
