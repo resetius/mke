1c5ad4a66be1ac16d3818dc2bcc097c9f9ce4102
Alexey Ozeritsky <aozeritsky@gmail.com>
2009-06-02 16:56:15 +0400

    --

--- utils/netcdf_cmd.h
+++ utils/netcdf_cmd.h
@@ -1,15 +1,23 @@
 #ifndef NETCDF_CMD_H
 #define NETCDF_CMD_H
 
+#include <netcdf.hh>
 #include <string.h>
 
 struct CMD_Parser {
 	int cur_;
 	int argc_;
 	char ** argv_;
+	NcFile * f_;
 
-	CMD_Parser (int argc, char * argv[]): cur_(0), argc_(argc), argv_(argv) {}
+	CMD_Parser (int argc, char * argv[]): f_(0), cur_(0), argc_(argc), argv_(argv) {}
+	~CMD_Parser() { delete f_; }
+
+	void parse();
+	void open(const char * name);
 	int next();
+
+	void info();
 
 	void info_dim(int number) {}
 	void info_dim(const char * name) {}
--- utils/netcdf_reader.cpp
+++ utils/netcdf_reader.cpp
@@ -1,40 +1,69 @@
-#include <netcdf.hh>
+#include "netcdf_cmd.h"
+#include "netcdf_cmd.hpp"
 
-void info_file(const char * path)
+void CMD_Parser::info()
 {
-	NcFile f(path);
-	if (!f.is_valid()) {
-		fprintf(stderr, "bad file %s\n", path);
-		return;
+	if (!f_) {
+		fprintf(stderr, "give me filename first!\n");
+		exit(-1);
 	}
 
-	int dims = f.num_dims();
-	int vars = f.num_vars();
-	int atts = f.num_atts();
+	int dims = f_->num_dims();
+	int vars = f_->num_vars();
+	int atts = f_->num_atts();
 
 	fprintf(stdout, "dims/vars/atts: %d/%d/%d\n", dims, vars, atts);
 	fprintf(stdout, "dims:\n");
 	for (int i = 0; i < dims; ++i) {
-		NcDim * dim = f.get_dim(i);
+		NcDim * dim = f_->get_dim(i);
 		if (dim) fprintf(stdout, "%d:\t%s: %ld\n", i, dim->name(), dim->size());
 	}
 
 	fprintf(stdout, "vars:\n");
 	for (int i = 0; i < vars; ++i) {
-		NcVar * var = f.get_var(i);
+		NcVar * var = f_->get_var(i);
 		if (var) fprintf(stdout, "%d:\t%s\n", i, var->name());
 	}
 
 	fprintf(stdout, "atts:\n");
 	for (int i = 0; i < vars; ++i) {
-		NcAtt * att = f.get_att(i);
+		NcAtt * att = f_->get_att(i);
 		if (att) fprintf(stdout, "%d:\t%s\n", i, att->name());
 	}
 }
 
+void CMD_Parser::open(const char * file)
+{
+	if (f_) { delete f_; f_ = 0; }
+	f_ = new NcFile(file);
+	if (f_->is_valid()) {
+		fprintf(stderr, "bad file %s\n", file);
+		exit(-1);
+	}
+}
+
+int CMD_Parser::next()
+{
+        if (cur_ == argc_) return 0;
+        yylval.str = argv_[cur_];
+        if (!strcmp(argv_[cur_], "help")) {
+                return HELP;
+        } else if (!strcmp(argv_[cur_], "info")) {
+                return INFO;
+        } else {
+                return yylval.str[0];
+        }
+        cur_ ++;
+}
+
+void CMD_Parser::parse()
+{
+}
+
 int main(int argc, char * argv[])
 {
-	info_file(argv[1]);
+	CMD_Parser p(argc, argv);
+	p.parse();
 	return 0;
 }
 
