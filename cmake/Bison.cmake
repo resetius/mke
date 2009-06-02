f76ee8866413c916333f39ea1b71ee2caaf9bf92
Alexey Ozeritsky <aozeritsky@gmail.com>
2009-06-02 13:23:28 +0400

    --

---
+++ cmake/Bison.cmake
@@ -0,0 +1,27 @@
+# flex a .ll file
+
+INCLUDE(FindBison)
+
+
+MACRO(ADD_BISON_FILES _sources )
+    FIND_PACKAGE(Bison QUIET REQUIRED)
+
+    FOREACH (_current_FILE ${ARGN})
+      GET_FILENAME_COMPONENT(_in ${_current_FILE} ABSOLUTE)
+      GET_FILENAME_COMPONENT(_basename ${_current_FILE} NAME_WE)
+
+      SET(_out ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.cpp)
+
+      ADD_CUSTOM_COMMAND(
+         OUTPUT ${_out}
+         COMMAND ${BISON_EXECUTABLE}
+         ARGS
+	 -d
+         -o${_out}
+         ${_in}
+         DEPENDS ${_in}
+      )
+
+      SET(${_sources} ${${_sources}} ${_out} )
+   ENDFOREACH (_current_FILE)
+ENDMACRO(ADD_BISON_FILES)
---
+++ utils/netcdf_cmd.h
@@ -0,0 +1,10 @@
+#ifndef NETCDF_CMD_H
+#define NETCDF_CMD_H
+
+struct CMD_Parser {
+};
+
+int yyparse(CMD_Parser * );
+
+#endif /* NETCDF_CMD_H */
+
