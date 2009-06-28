2a30c2bcd8c9d18b2f910a2cf168fdd07a621e54
Alexey Ozeritsky <aozeritsky@gmail.com>
2009-06-28 22:17:13 +0400

    --

--- src/gmres.h
+++ src/gmres.h
@@ -10,42 +10,42 @@
  *
  * @section LICENSE
  *
- * Copyright (c) 2009 Alexey Ozeritsky
+ * Copyright (c) 2009 Alexey Ozeritsky \n
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Redistributions in any form must be accompanied by information on
- *    how to obtain complete source code for the Phelm software and any
- *    accompanying software that uses the Phelm software.  The source code
- *    must either be included in the distribution or be available for no
- *    more than the cost of distribution plus a nominal fee, and must be
- *    freely redistributable under reasonable conditions.  For an
- *    executable file, complete source code means the source code for all
- *    modules it contains.  It does not include source code for modules or
- *    files that typically accompany the major components of the operating
- *    system on which the executable file runs.
+ * Redistribution and use in source and binary forms, with or without \n
+ * modification, are permitted provided that the following conditions \n
+ * are met: \n
+ * 1. Redistributions of source code must retain the above copyright \n
+ *    notice, this list of conditions and the following disclaimer. \n
+ * 2. Redistributions in binary form must reproduce the above copyright \n
+ *    notice, this list of conditions and the following disclaimer in the \n
+ *    documentation and/or other materials provided with the distribution. \n
+ * 3. Redistributions in any form must be accompanied by information on \n
+ *    how to obtain complete source code for the Phelm software and any \n
+ *    accompanying software that uses the Phelm software.  The source code \n
+ *    must either be included in the distribution or be available for no \n
+ *    more than the cost of distribution plus a nominal fee, and must be \n
+ *    freely redistributable under reasonable conditions.  For an \n
+ *    executable file, complete source code means the source code for all \n
+ *    modules it contains.  It does not include source code for modules or \n
+ *    files that typically accompany the major components of the operating \n
+ *    system on which the executable file runs. \n
  *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR \n
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES \n
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. \n
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, \n
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT\n
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE \n,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY \n
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT \n
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF \n
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. \n
  *
  * @section DESCRIPTION
  *
- * Generalized minimal residual method (GMRES)
+ * Generalized minimal residual method (GMRES) \n
  * http://en.wikipedia.org/wiki/GMRES
  */
 
