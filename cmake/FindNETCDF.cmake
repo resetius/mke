346d21ca5e60b6837aab863d42552f51fa153937
Alexey Ozeritsky <aozeritsky@gmail.com>
2009-12-13 01:27:59 +0300

    link fix

--- cmake/FindNETCDF.cmake
+++ cmake/FindNETCDF.cmake
@@ -6,13 +6,19 @@ find_path(NETCDF_INCLUDE_DIR netcdf.hh
 	/usr/local/include 
 	/usr/include)
 
-find_library(NETCDF_LIBRARIES netcdf_c++ HINTS 
+find_library(NETCDF_LIB1 NAMES netcdf_c++ HINTS 
 	/usr/lib 
 	/usr/lib64
 	/usr/local/lib
 	/usr/local/lib64
 )
-
+find_library(NETCDF_LIB2 NAMES netcdf HINTS 
+	/usr/lib 
+	/usr/lib64
+	/usr/local/lib
+	/usr/local/lib64
+)
+set(NETCDF_LIBRARIES ${NETCDF_LIB1};${NETCDF_LIB2})
 if (NETCDF_INCLUDE_DIR AND NETCDF_LIBRARIES)
 set (NETCDF_FOUND 1)
 message(STATUS "netcdf: ${NETCDF_LIBRARIES}")
