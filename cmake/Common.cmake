1d14913910fce0b318a95ce1be5f79ce64d9e641
Alexey Ozeritsky <aozeritsky@gmail.com>
2010-04-18 21:30:41 +0400

    fix paths2

--- cmake/Common.cmake
+++ cmake/Common.cmake
@@ -1,4 +1,6 @@
 if(COMMAND cmake_policy)
 	cmake_policy(SET CMP0003 NEW)
 endif(COMMAND cmake_policy)
+set(LIBRARY_OUTPUT_PATH "${CMAKE_BINARY_DIR}/lib")
+set(EXECUTABLE_OUTPUT_PATH "${CMAKE_BINARY_DIR}/bin")
 
