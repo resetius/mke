cf26fa049e0cff770a35e252a19aae14a4008932
Alexey Ozeritsky <aozeritsky@gmail.com>
2010-06-01 15:52:39 +0400

    mke and sp support separate build

--- cmake/CheckedAddSubdir.cmake
+++ cmake/CheckedAddSubdir.cmake
@@ -1,7 +1,7 @@
 
 macro (add_subdirectory_if_exists _subdir)
-	if (EXISTS "./${_subdir}")
+	if (EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${_subdir})
 		add_subdirectory (${_subdir})
-	endif (EXISTS "./${_subdir}")
+	endif ()
 endmacro(add_subdirectory_if_exists)
 
