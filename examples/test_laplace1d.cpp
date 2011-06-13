98ee9373bb0ff053c490585f984649e47b6cf5fb
Alexey Ozeritsky <aozeritsky@gmail.com>
2011-06-13 22:09:07 +0400

    update simple 1d example

--- examples/test_laplace1d.cpp
+++ examples/test_laplace1d.cpp
@@ -22,10 +22,6 @@ static double lapl_func(double x)
 	return 2.0;
 }
 
-static void calc_lapl()
-{
-}
-
 static double fx(double x, double * d)
 {
 	//return (x - d->xl) * (-x + d->xc);
@@ -56,6 +52,88 @@ static double fx2(double x, struct fx_data * d)
 		throw "error";
 	}
 	return 0.0;
+}
+
+static void calc_lapl()
+{
+	double x0  = -2.0;
+	double x1  =  2.0;
+	int points =  5; // including edges
+	int rs     = points - 2; // inner points
+	double dx  = (x1 - x0) / (points - 1);
+
+	linal::SparseSolver < double, 
+		linal::StoreCSR < double , linal::Allocator > , 
+		linal::StoreCSR < double , linal::Allocator > > A(rs);
+	vector < double > rp(rs);
+	vector < double > ans(rs);
+	vector < double > rans(rs);
+
+	// inner points
+	for (int i = 0; i < rs; ++i) {
+		int p = i + 1;
+
+		double xc = p * dx + x0;
+		double xl = (p - 1) * dx + x0;
+		double xr = (p + 1) * dx + x0;
+		double a;
+		double b;
+
+		int p2 = p;
+		int j  = p2 - 1;
+		double k;
+
+		fx_data data;
+		data.xc = xc;
+		data.xl = xl;
+		data.xr = xr;
+
+		rans[i] = lapl_func(xc);
+
+		k  = 1.0;
+		b  = gauss_kronrod15(xl, xc, (fx_t)fx, &k);
+		b += gauss_kronrod15(xc, xr, (fx_t)fx, &k);
+		rp[i] = -func(xc) * b;
+
+		data.type = 0;
+		a  = gauss_kronrod15(xl, xc, (fx_t)fx2, &data);
+		A.add(i, j, a);
+		data.type = 1;
+		a  = gauss_kronrod15(xc, xr, (fx_t)fx2, &data);
+		A.add(i, j, a);
+
+		p2 = p - 1;
+		j  = p2 - 1;
+		k  = -1.0;
+
+		b  = gauss_kronrod15(xl, xc, (fx_t)fx, &k);
+		rp[i] += -func(xl) * b;
+
+		data.type = 2;
+		a  = gauss_kronrod15(xl, xc, (fx_t)fx2, &data);
+		if (j >= 0) {
+			A.add(i, j, a);
+		} else {
+		}
+
+		p2 = p + 1;
+		j  = p2 - 1;
+
+		b = gauss_kronrod15(xc, xr, (fx_t)fx, &k);
+		rp[i] += -func(xr) * b;
+
+		data.type = 3;
+		a  = gauss_kronrod15(xc, xr, (fx_t)fx2, &data);
+		if (j < rs) {
+			A.add(i, j, a);
+		} else {
+		}
+	}
+
+	A.print(stderr);
+	A.solve(&ans[0], &rp[0]);
+
+	fprintf(stderr, "done\n");
 }
 
 static void solve_lapl()
@@ -156,5 +234,6 @@ static void solve_lapl()
 
 int main(int argc, char ** argv)
 {
-	solve_lapl();
+	//solve_lapl();
+	calc_lapl();
 }
