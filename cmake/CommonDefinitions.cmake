2fb8d3585d84b7ae3d49355bb8818bcfcf3b9ad1
Alexey Ozeritsky <aozeritsky@gmail.com>
2015-08-23 01:18:42 +0300

    fixes #10: simplify openmp cmake rules

--- cmake/CommonDefinitions.cmake
+++ cmake/CommonDefinitions.cmake
@@ -2,6 +2,9 @@ if (MSVC)
         add_definitions(/D_CRT_SECURE_NO_WARNINGS)
 endif (MSVC)
 
-add_definitions(${OpenMP_C_FLAGS})
+if (OPENMP_FOUND)
+	set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
+	set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
+endif()
 add_definitions(${LINAL_DEFINES})
 
